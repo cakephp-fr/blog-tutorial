1b768c36fce3fcec2430926f2ae8a35a587aee17
cake17 <cake17@cake-websites.com>
2016-07-21 17:08:37 +0200

    part4

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -39,10 +39,29 @@ class AppController extends Controller
      */
     public function initialize()
     {
-        parent::initialize();
-
         $this->loadComponent('RequestHandler');
         $this->loadComponent('Flash');
+        $this->loadComponent('Auth', [
+            'authenticate' => [
+                'Form' => [
+                    'fields' => [
+                        'username' => 'username',
+                        'password' => 'password'
+                    ]
+                ]
+            ],
+            'loginRedirect' => [
+                'controller' => 'Articles',
+                'action' => 'index'
+            ],
+            'logoutRedirect' => [
+                'controller' => 'Pages',
+                'action' => 'display',
+                'home'
+            ]
+        ]);
+
+        $this->Auth->allow(['index', 'view', 'display']);
     }
 
     /**
